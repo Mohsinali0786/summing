535a06580e42e8fd3b182b05a971a2766a41328a
mohsinali-iot <mohsinali481997@gmail.com>
2019-10-14 15:34:23 +0500

    second commit

---
+++ sum/target/package/sum-0.1.0/target/debug/deps/sum-a143964711b8a026.d
@@ -0,0 +1,7 @@
+/home/mohsin/piaic/summing/sum/target/package/sum-0.1.0/target/debug/deps/sum-a143964711b8a026.rmeta: src/lib.rs
+
+/home/mohsin/piaic/summing/sum/target/package/sum-0.1.0/target/debug/deps/libsum-a143964711b8a026.rlib: src/lib.rs
+
+/home/mohsin/piaic/summing/sum/target/package/sum-0.1.0/target/debug/deps/sum-a143964711b8a026.d: src/lib.rs
+
+src/lib.rs:
---
+++ sum/target/package/sum-0.1.0/target/debug/libsum.d
@@ -0,0 +1,1 @@
+/home/mohsin/piaic/summing/sum/target/package/sum-0.1.0/target/debug/libsum.rlib: /home/mohsin/piaic/summing/sum/target/package/sum-0.1.0/src/lib.rs
